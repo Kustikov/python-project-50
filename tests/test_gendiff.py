0e04a92d79e594e9be4d3f12a6f024bfe69017d9
kustikov <pasha_k1987@mail.ru>
2023-04-06 14:54:03 +0300

    add tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,12 +1,17 @@
 from gendiff import generate_diff
 
-path1 = './gendiff/json_files/first_file.json'
-path2 = './gendiff/json_files/second_file.json'
+
+path1 = './tests/fixtures/first_file.json'
+path2 = './tests/fixtures/second_file.json'
 
 
 def test_diff():
     diff = generate_diff(path1, path2)
     assert type(diff) == str
+    assert len(path1) > 0
+    assert len(path2) > 0
+    assert path1[-4:] == 'json'
+    assert path2[-4:] == 'json'
     assert diff == '{\n + follow: False\n   host: hexlet.io\n + proxy: 123.234.53.22\n + timeout: 50\n - timeout: 20\n - verbose: True\n}'
     
     
