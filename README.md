22745a27781f631b16982f6b8cd59688c7db3337
kustikov <pasha_k1987@mail.ru>
2023-04-10 14:41:20 +0300

    add asciinema for yaml formate

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/b1c5ca51eae2bfda2ee9/maintainability)](https://codeclimate.com/github/Kustikov/python-project-50/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/b1c5ca51eae2bfda2ee9/test_coverage)](https://codeclimate.com/github/Kustikov/python-project-50/test_coverage)
 
-### Gendiff script example:
+### Gendiff for json and yaml:
 [![asciicast](https://asciinema.org/a/09UL6X4a4JUqUTdm0Y4s2gIqd.svg)](https://asciinema.org/a/09UL6X4a4JUqUTdm0Y4s2gIqd)
+[![asciicast](https://asciinema.org/a/RfIBOeiG8Hvejj1LKecCOKi7d.svg)](https://asciinema.org/a/RfIBOeiG8Hvejj1LKecCOKi7d)
--- gendiff/modules/gendiff.py
+++ gendiff/modules/gendiff.py
@@ -22,16 +22,16 @@ def generate_diff(path1, path2):
     second_file_item = dict_2.items()
     diff_items_1 = first_file_item - second_file_item
     for key, value in diff_items_1:
-        result.append(f"{plus} {key}: {value}")
+        result.append(f"{minus} {key}: {value}")
     diff_items_2 = second_file_item - first_file_item
     for key, value in diff_items_2:
-        result.append(f"{minus} {key}: {value}")
+        result.append(f"{plus} {key}: {value}")
     compare_items = first_file_item & second_file_item
     for key, value in compare_items:
         result.append(f"{space} {key}: {value}")
     second_index = 2  # фильтруем по второму индексу
-    last_index = -1  # фильтруем до последнего индекса
-    result.sort(key=lambda by_name: by_name[second_index:last_index])
+    # last_index = -1  # фильтруем до последнего индекса
+    result.sort(key=lambda by_name: by_name[second_index])
     new_result = ""
     for i in result:
         new_result = f"{new_result} {i}\n"
