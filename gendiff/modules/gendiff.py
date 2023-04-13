53adb3a6e4efc568d4cd7e96e90c91d920a5360d
kustikov <pasha_k1987@mail.ru>
2023-04-13 18:54:57 +0300

    change logic of gendiff and create build tree from files

--- gendiff/modules/gendiff.py
+++ gendiff/modules/gendiff.py
@@ -11,57 +11,44 @@ def get_dict(path):
             return yaml.safe_load(file)
 
 
-def generate_diff(path1, path2):
+def generate_diff(path1, dict_2):
+    result = []
     dict_1 = get_dict(path1)
-    dict_2 = get_dict(path2)
-    result = {}
-    plus = "+"
-    minus = "-"
-    space = " "
-    first_file_item = dict_1.items()
-    second_file_item = dict_2.items()
-    # diff_items_1 = first_file_item - second_file_item
-    # for key, value in diff_items_1:
-    #     result.append(f"{minus} {key}: {value}")
-    # diff_items_2 = second_file_item - first_file_item
-    # for key, value in diff_items_2:
-    #     result.append(f"{plus} {key}: {value}")
-    # compare_items = first_file_item & second_file_item
-    # for key, value in compare_items:
-    #     result.append(f"{space} {key}: {value}")
-    # second_index = 2  # фильтруем по второму индексу
-    # # last_index = -1  # фильтруем до последнего индекса
-    # result.sort(key=lambda by_name: by_name[second_index])
-    # new_result = ""
-    # for i in result:
-    #     new_result = f"{new_result} {i}\n"
-    # print(new_result)
-    # return f"{{\n{new_result}}}"
-    diff_items_1 = first_file_item - second_file_item
-    for key, value in diff_items_1:
-        #result.append(f"{minus} {key}: {value}")
-        result.setdefault(minus, []).append(f'{key}: {value}')
-    diff_items_2 = second_file_item - first_file_item
-    for key, value in diff_items_2:
-        result.setdefault(plus, []).append(f'{key}: {value}')
-    compare_items = first_file_item & second_file_item
-    for key, value in compare_items:
-        result.setdefault(space, []).append(f'{key}: {value}')
+    dict_2 = get_dict(dict_2)
+    result = []
+    keys_1 = dict_1.keys()
+    keys_2 = dict_2.keys()
+    keys = sorted(dict_1.keys() | dict_2.keys())
+    for key in keys:
+        if key not in keys_1:
+            result.append({"name": key, "value": dict_2[key], "type": "added"})
+        elif key not in keys_2:
+            result.append({"name": key, "value": dict_1[key], "type": "deleted"})
+        elif dict_1[key] == dict_2[key]:
+            result.append({"name": key, "value": dict_1[key], "type": "unchanged"})
+        elif dict_1[key] != dict_2[key]:
+            result.append(
+                {
+                    "name": key,
+                    "value1": dict_1[key],
+                    "value2": dict_2[key],
+                    "type": "changed",
+                }
+            )
     print(result)
     return result
 
 
-# def formatter(generate_diff, get_dict):
-#     result = ''
-#     dict = generate_diff(dict1, dict2)
-#     for key in dict:
-#         for i in dict[key]:
-#             result = f'{result}{key} {i}\n'
-#     result = result.split('\n')
-#     start_index = 2
-#     end_index = 3
-#     result.sort(key=lambda by_name: by_name[start_index:end_index])
-#     answer = '\n'.join(result)
-#     return f'{{{answer}\n}}'
-
-
+def formatter(diff):
+    print(diff)
+    result = ""
+    for key in diff:
+        for i in diff[key]:
+            result = f"{result}{key} {i}\n"
+    result = result.split("\n")
+    start_index = 2
+    end_index = 3
+    result.sort(key=lambda by_name: by_name[start_index:end_index])
+    answer = "\n".join(result)
+    print(f"{{{answer}\n}}")
+    return answer
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -10,22 +10,22 @@ path4 = 'tests/fixtures/second_file.yaml'
 
 def test_diff():
     diff = generate_diff(path1, path2)
-    assert type(diff) == str
+    assert type(diff) == list
     assert len(path1) > 0
     assert len(path2) > 0
     assert path1[-4:] == 'json'
     assert path2[-4:] == 'json'
-    assert diff == '{\n - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: True\n}'
+    assert diff == [{'name': 'follow', 'value': False, 'type': 'deleted'}, {'name': 'host', 'value': 'hexlet.io', 'type': 'unchanged'}, {'name': 'proxy', 'value': '123.234.53.22', 'type': 'deleted'}, {'name': 'timeout', 'value1': 50, 'value2': 20, 'type': 'changed'}, {'name': 'verbose', 'value': True, 'type': 'added'}]
 
 
 def test_diff():
     diff = generate_diff(path3, path4)
-    assert type(diff) == str
+    assert type(diff) == list
     assert len(path1) > 0
     assert len(path2) > 0
     assert path1[-4:] == 'json'
     assert path2[-4:] == 'json'
-    assert diff == '{\n - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: True\n}'
+    assert diff == [{'name': 'follow', 'value': False, 'type': 'deleted'}, {'name': 'host', 'value': 'hexlet.io', 'type': 'unchanged'}, {'name': 'proxy', 'value': '123.234.53.22', 'type': 'deleted'}, {'name': 'timeout', 'value1': 50, 'value2': 20, 'type': 'changed'}, {'name': 'verbose', 'value': True, 'type': 'added'}]
 
 
 
